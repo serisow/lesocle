2cd9c028587cb03578a3e61c8f9d253de3465d33
serisow <serisow@gmail.com>
2025-02-28 15:22:27 +0100

    Refactor+clean js code

--- web/modules/custom/pipeline/js/pipeline-step-type-modal.js
+++ web/modules/custom/pipeline/js/pipeline-step-type-modal.js
@@ -25,12 +25,6 @@
           formData.append('step_type', stepType); // Add this line
           formData.append('uuid', uuid); // Add this line
 
-          // For Multiple Choice steps, add options to formData
-          if (stepType === 'multiple_choice') {
-            var options = Drupal.multipleChoiceStep.getOptions($form);
-            formData.append('options', JSON.stringify(options));
-          }
-
           $.ajax({
             url: formAction,
             type: 'POST',
@@ -60,17 +54,9 @@
                         $('.ui-dialog-content').html(command.data);
                         Drupal.attachBehaviors($('.ui-dialog-content')[0]);
                       }
-                      if (command.method === 'replaceWith' && command.selector === '#multiple-choice-options-wrapper') {
-                        $('#multiple-choice-options-wrapper').html(command.data);
-                        Drupal.attachBehaviors($('#multiple-choice-options-wrapper')[0]);
-                      }
                       break;
                   }
                 });
-                // For Multiple Choice steps, handle specific response
-                if (stepType === 'multiple_choice') {
-                  Drupal.multipleChoiceStep.handleResponse(response);
-                }
               }
 
               // Keep these checks for backwards compatibility or if the response structure changes
@@ -87,33 +73,6 @@
             }
           });
         });
-
-        // Handle add option button click for Multiple Choice steps
-        $(element).on('click', '.ui-dialog[class*="pipeline-step-type-add-modal"] .add-option', function (e) {
-          e.preventDefault();
-          var $button = $(this);
-          var $form = $button.closest('form');
-          var pipelineId = $button.data('pipeline-id') || drupalSettings.pipelineId;
-          var stepType = $button.data('step-type') || $form.find('input[name="step_type"]').val();
-          var formId = $form.attr('id');
-
-          var options = Drupal.multipleChoiceStep.getOptions($form);
-
-          $.ajax({
-            url: Drupal.url('admin/structure/pipelines/' + pipelineId + '/add-multiple-choice-option'),
-            type: 'POST',
-            data: {
-              step_type: stepType,
-              options: JSON.stringify(options),
-              form_id: formId
-            },
-            dataType: 'json',
-            success: function (response) {
-              Drupal.multipleChoiceStep.handleResponse(response);
-            }
-          });
-        });
-
         // Handle prompt template selection
         $(element).on('autocompleteclose', 'input[name="data[prompt_template]"]', function (event) {
           var $input = $(this);
@@ -146,142 +105,9 @@
           }
         });
 
-        // Handle file uploads for image and audio fields
-        $(element).on('change', 'input[type="file"][name^="files[data_"]', function(event) {
-          var $fileInput = $(this);
-          var $form = $fileInput.closest('form');
-          var pipelineId = drupalSettings.pipelineId;
-          var stepType = $form.find('input[name="step_type"]').val();
-          var uuid = $form.find('input[name="uuid"]').val();
-          var fieldName = $fileInput.attr('name').match(/files\[(.*?)\]/)[1]; // Extract field name
-
-          // Don't proceed if no file selected
-          if (!this.files || !this.files[0]) {
-            return;
-          }
-
-          // Show upload in progress message
-          var $wrapper = $fileInput.closest('.form-managed-file');
-          if ($wrapper.find('.file-upload-processing').length === 0) {
-            $wrapper.append('<div class="file-upload-processing">Uploading file...</div>');
-          }
-
-          // Create FormData for the file upload
-          var formData = new FormData();
-          formData.append('files[' + fieldName + ']', this.files[0]);
-          formData.append('pipeline_id', pipelineId);
-          formData.append('step_type', stepType);
-          formData.append('field_name', fieldName);
-          if (uuid) {
-            formData.append('uuid', uuid);
-          }
-
-          // Send AJAX request to upload the file
-          $.ajax({
-            url: drupalSettings.path.baseUrl + 'admin/structure/pipelines/file-upload',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            dataType: 'json',
-            success: function(response) {
-              // Remove processing message
-              $wrapper.find('.file-upload-processing').remove();
-
-              if (response.status === 'success') {
-                // Update the form with the file information
-                Drupal.behaviors.pipelineStepTypeModal.updateFileField($wrapper, response);
-              } else {
-                console.error('File upload error:', response.message);
-                Drupal.behaviors.pipelineStepTypeModal.showMessage(response.message, 'error');
-              }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-              $wrapper.find('.file-upload-processing').remove();
-              console.error('AJAX error:', textStatus, errorThrown);
-              Drupal.behaviors.pipelineStepTypeModal.showMessage('An error occurred during file upload.', 'error');
-            }
-          });
-        });
-
-        // Handle remove file button clicks
-        $(element).on('click', '.form-managed-file .remove-file', function(event) {
-          event.preventDefault();
-          var $button = $(this);
-          var $wrapper = $button.closest('.form-managed-file');
-
-          // Reset the file input and remove the file display
-          $wrapper.find('input[type="file"]').val('');
-          $wrapper.find('.file-display').remove();
-          $wrapper.find('input[name$="[fids]"]').val('');
-
-          // Show the file upload input again
-          $wrapper.find('input[type="file"]').show();
-        });
-
       });
     },
 
-    updateFileField: function($wrapper, response) {
-      // Hide the file input
-      $wrapper.find('input[type="file"]').hide();
-
-      // Add or update the hidden input for the file ID
-      var fieldName = response.field_name;
-      var inputName = fieldName + '[fids]';
-
-      if ($wrapper.find('input[name="' + inputName + '"]').length === 0) {
-        $wrapper.append('<input type="hidden" name="' + inputName + '" value="' + response.fid + '">');
-      } else {
-        $wrapper.find('input[name="' + inputName + '"]').val(response.fid);
-      }
-
-      // Determine file type class based on mime type
-      var mimeClass = 'file--mime-' + response.mime.replace('/', '-');
-      var fileTypeClass = '';
-
-      // Add general file type class
-      if (response.mime.indexOf('image/') === 0) {
-        fileTypeClass = 'file--image';
-      } else if (response.mime.indexOf('audio/') === 0) {
-        fileTypeClass = 'file--audio';
-      } else if (response.mime.indexOf('video/') === 0) {
-        fileTypeClass = 'file--video';
-      } else {
-        fileTypeClass = 'file--general';
-      }
-
-      // Create or update the file display to match Drupal's managed_file style exactly
-      var fileUrl = response.url;
-
-      var fileDisplay =
-        '<div class="js-form-managed-file form-managed-file is-single no-upload has-value no-meta">' +
-        '<div class="form-managed-file__main">' +
-        '<span data-drupal-selector="edit-' + fieldName.replace(/_/g, '-') + '-file-' + response.fid + '-filename" class="file ' + mimeClass + ' ' + fileTypeClass + '">' +
-        '<a href="' + fileUrl + '" type="' + response.mime + '">' + response.filename + '</a>' +
-        ' <span class="file__size">(' + response.filesize + ')</span>' +
-        '</span>' +
-        '<input data-drupal-selector="edit-' + fieldName.replace(/_/g, '-') + '-file-' + response.fid + '-remove-button" formnovalidate="formnovalidate" ' +
-        'class="button--extrasmall remove-button button js-form-submit form-submit" ' +
-        'type="submit" name="' + fieldName + '_remove_button" value="Remove">' +
-        '</div>' +
-        '</div>';
-
-      // Check if we need to replace an existing display or create a new one
-      if ($wrapper.find('.form-managed-file').length === 0) {
-        $wrapper.append(fileDisplay);
-      } else {
-        $wrapper.find('.form-managed-file').replaceWith(fileDisplay);
-      }
-
-      // Reattach the click handler for the remove button
-      $wrapper.find('.remove-button').on('click', function(event) {
-        event.preventDefault();
-        $wrapper.find('input[type="file"]').val('').show();
-        $wrapper.find('.form-managed-file').remove();
-        $wrapper.find('input[name="' + inputName + '"]').val('');
-      });
-    },
     showFormErrors: function(errors) {
       $('.ui-dialog-content .form-item--error-message').remove();
       $('.ui-dialog-content .form-item--error').removeClass('form-item--error');
@@ -327,10 +153,4 @@
     }
   };
 
-  Drupal.AjaxCommands.prototype.updateRemainingOptions = function(ajax, response, status) {
-    if (response.data) {
-      $('#multiple-choice-options-wrapper').replaceWith(response.data);
-      Drupal.attachBehaviors($('#multiple-choice-options-wrapper')[0]);
-    }
-  };
 })(jQuery, Drupal, once);
